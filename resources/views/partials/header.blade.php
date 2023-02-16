5f4ffc033ca63397f2d4ab4c79cccfef1742573b
Luigi213 <reyesgianluigi88@gmail.com>
2023-02-16 17:46:11 +0100

    fix header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,17 +5,17 @@
                 <div class="image">
                     <img src="img/dc-logo.png" alt="logo">
                 </div>
-                <ul>
-                    <li class="active">CHARACTERS</li>
-                    <li>COMICS</li>
-                    <li>MOVIES</li>
-                    <li>TV</li>
-                    <li>GAMES</li>
-                    <li>COLLECTIBLES</li>
-                    <li>VIDEOS</li>
-                    <li>FANS</li>
-                    <li>NEWS</li>
-                    <li>SHOP</li>
+                <ul class="m-0">
+                    <li><a href="/">CHARACTERS</a> </li>
+                    <li><a href="/">COMICS</a> </li>
+                    <li><a href="/">MOVIES</a> </li>
+                    <li><a href="/">TV</a> </li>
+                    <li><a href="/">GAMES</a> </li>
+                    <li><a href="/">COLLECTIBLES</a> </li>
+                    <li><a href="/">VIDEOS</a> </li>
+                    <li><a href="/">FANS</a> </li>
+                    <li><a href="/">NEWS</a> </li>
+                    <li><a href="/">SHOP</a> </li>
                 </ul>
             </div>
         </div>
